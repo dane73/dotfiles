db0822327e01ecd9fed46b90d6c8f38346d790b1
dane73 <116499574+dane73@users.noreply.github.com>
2025-08-14 23:10:23 +0200

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,162 @@
-require("config")
+-- settings
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.winborder = "rounded"
+vim.opt.mouse = ""
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.scrolloff = 8
+vim.opt.colorcolumn = "80,100"
+vim.opt.signcolumn = "yes:2"
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+vim.api.nvim_create_user_command("Config", function()
+    vim.cmd("edit " .. vim.fn.stdpath("config") .. "/init.lua")
+end, { desc = "Open neovim config" })
+
+-- keymaps
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", vim.cmd.write)
+vim.keymap.set("n", "<C-d", "<C-d>zz")
+vim.keymap.set("n", "<C-u", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<space>", '<Nop>', { silent = true })
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
+vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { desc = 'Signature help' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open diagnostic message' })
+
+-- lsp config
+vim.lsp.config['luals'] = {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true)
+            }
+        }
+    }
+}
+local root_markers_python = {
+    'pyproject.toml',
+    'setup.py',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+    'pyrightconfig.json',
+    '.git',
+}
+vim.lsp.config['basedpyright'] = {
+    cmd = { 'basedpyright-langserver', '--stdio' },
+    filetypes = { 'python' },
+    root_markers = root_markers_python,
+    settings = {
+        basedpyright = {
+            analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = 'openFilesOnly',
+                diasbleOrganizeImports = true,
+            },
+        },
+    },
+}
+vim.lsp.config['ruff'] = {
+    cmd = { 'ruff', 'server'},
+    filetypes = { 'python' },
+    root_markers = root_markers_python,
+    settings = {}
+}
+vim.lsp.enable({ 'luals', 'basedpyright', 'ruff'})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = 'LSP: Disable hover capability from Ruff',
+})
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- plugins
+require("lazy").setup({
+    { "rose-pine/neovim",             name = "rose-pine",      priority = 1000 },
+    { "f-person/auto-dark-mode.nvim", opts = { os = "macos" }, },
+    { 'lewis6991/gitsigns.nvim', },
+    { 'ThePrimeagen/harpoon',         branch = 'harpoon2',     dependencies = { "nvim-lua/plenary.nvim" } },
+})
+
+-- set colorscheme
+if vim.opt.background:get() == "dark" then
+    vim.cmd.colorscheme("rose-pine")
+else
+    vim.cmd.colorscheme("rose-pine-dawn")
+end
+
+require("gitsigns").setup({
+    signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '-' },
+        topdelete = { text = '-' },
+        changedelete = { text = '~' },
+        untracked = { text = '?' },
+    },
+})
+require('harpoon'):setup()
+
+-- plugin keymaps
+local harpoon = require('harpoon')
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
